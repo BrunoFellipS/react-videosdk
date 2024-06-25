eb0d5f9a43b4db665bc8e870fad86c9a65b6147d
Bruno Fellip Silva <brunofellip.silva@gmail.com>
2024-06-25 00:43:50 -0300

    att

--- src/App.js
+++ src/App.js
@@ -1,9 +1,135 @@
+import React, { useEffect, useMemo, useRef, useState } from "react";
+import {
+  MeetingProvider,
+  MeetingConsumer,
+  useMeeting,
+  useParticipant,
+} from "@videosdk.live/react-sdk";
+import ReactPlayer from "react-player";
 
+function JoinScreen({ getMeetingAndToken }) {
+  const [meetingId, setMeetingId] = useState(null);
+  const webcamRef = useRef(null);
+  const micRef = useRef(null);
+
+  
+  
+  const onClick = async () => {
+    await getMeetingAndToken(meetingId);
+  };
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="Enter Meeting Id"
+        onChange={(e) => {
+          setMeetingId(e.target.value);
+        }}
+      />
+      <button onClick={onClick}>Join</button>
+      {" or "}
+      <button onClick={onClick}>Create Meeting</button>
+    </div>
+  );
+}
+
+function ParticipantView(props) {
+  const { webcamStream, micStream, webcamOn, micOn } = useParticipant(
+    props.participantId
+  );
+}
+
+function Controls() {
+  const { leave, toggleMic, toggleWebcam } = useMeeting();
+  return (
+    <div>
+      <button onClick={() => leave()}>Leave</button>
+      <button onClick={() => toggleMic()}>toggleMic</button>
+      <button onClick={() => toggleWebcam()}>toggleWebcam</button>
+    </div>
+  );
+}
+
+function MeetingView(props) {
+  const [joined, setJoined] = useState(null);
+  //Get the method which will be used to join the meeting.
+  //We will also get the participants list to display all participants
+  const { join, participants } = useMeeting({
+    //callback for when meeting is joined successfully
+    onMeetingJoined: () => {
+      setJoined("JOINED");
+    },
+    //callback for when meeting is left
+    onMeetingLeft: () => {
+      props.onMeetingLeave();
+    },
+  });
+  const joinMeeting = () => {
+    setJoined("JOINING");
+    join();
+  };
+
+  return (
+    <div className="container">
+      <h3>Meeting Id: {props.meetingId}</h3>
+      {joined && joined == "JOINED" ? (
+        <div>
+          <Controls />
+          //For rendering all the participants in the meeting
+          {[...participants.keys()].map((participantId) => (
+            <ParticipantView
+              participantId={participantId}
+              key={participantId}
+            />
+          ))}
+        </div>
+      ) : joined && joined == "JOINING" ? (
+        <p>Joining the meeting...</p>
+      ) : (
+        <button onClick={joinMeeting}>Join</button>
+      )}
+    </div>
+  );
+}
 
 function App() {
-  return (
-    <div className="App">
-    </div>
+  const [meetingId, setMeetingId] = useState(null);
+  const [authToken, setAuthToken] = useState(null);
+
+  const getMeetingAndToken = async (id) => {
+    const {token} = await fetch('/api/token').then((res) => res.json());
+    setAuthToken(token);
+
+    if (id) {
+      setMeetingId(id);
+    } else {
+      const {roomId} = await fetch('/api/rooms', {
+        method: 'POST'
+      }).then((res) => res.json());
+
+      setMeetingId(roomId);
+    }
+  };
+
+  //This will set Meeting Id to null when meeting is left or ended
+  const onMeetingLeave = () => {
+    setMeetingId(null);
+  };
+
+  return authToken && meetingId ? (
+    <MeetingProvider
+      config={{
+        meetingId,
+        micEnabled: true,
+        webcamEnabled: true,
+        name: 'C.V. Raman',
+      }}
+      token={authToken}
+    >
+      <MeetingView meetingId={meetingId} onMeetingLeave={onMeetingLeave} />
+    </MeetingProvider>
+  ) : (
+    <JoinScreen getMeetingAndToken={getMeetingAndToken} />
   );
 }
 
--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,4 +2,18 @@ module.exports = app => {
     app.get('/api/token', (req, res) => {
         res.json({ token: process.env.VIDEOSDK_KEY });
     });
+
+    app.post('/api/rooms', async (req, res) => {
+      const room = await fetch(`https://api.videosdk.live/v2/rooms`, {
+        method: "POST",
+        headers: {
+          authorization: process.env.VIDEOSDK_KEY,
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({}),
+      });
+
+      const { roomId } = await room.json();
+      res.json({ roomId });
+    });
   }
